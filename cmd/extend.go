298f4f7359389e4731759676f534bc119145a964
小刘刘刘 <lcj3535@163.com>
2023-08-05 22:15:53 +0800

    first commit

--- cmd/extend.go
+++ cmd/extend.go
@@ -3,9 +3,11 @@ package cmd
 var ExtConfig Extend
 
 // Extend 扩展配置
-//  extend:
-//    demo:
-//      name: demo-name
+//
+//	extend:
+//	  demo:
+//	    name: demo-name
+//
 // 使用方法： config.ExtConfig......即可！！
 type Extend struct {
 	AMap AMap // 这里配置对应配置文件的结构即可
